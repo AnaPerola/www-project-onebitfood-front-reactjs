fc458c7e825cf6f6eacb51bbc55e1fe667c2b98e
AnaPerola <anaperolinha@hotmail.com>
2021-06-06 15:55:21 -0300

    Fix render ssg

--- pages/restaurants/[id].js
+++ pages/restaurants/[id].js
@@ -1,7 +1,25 @@
 import DetailsRestaurant from '../../components/DetailsRestaurant';
 
-export default function Restaurant(){
+export default function Restaurant({restaurant}){
   return(
-    <DetailsRestaurant />
+    <DetailsRestaurant restaurant={restaurant}/>
   )
+}
+
+export async function getStaticPaths({params}){
+  const res = await fetch(`${process.env.apiUrl}/api/restaurants`);
+  const restaurants = await res.json();
+  const paths = restaurants.map((restaurant) => ({
+    params: { id: restaurant.id.toString() }
+  }))
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({params}){
+  const res = await fetch(`${process.env.apiUrl}/api/restaurants/${params.id}`);
+  const restaurant = await res.json();
+  return {
+    props: { restaurant },
+    revalidate: 120
+  }
 }
